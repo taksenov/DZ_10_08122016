a9fbc54fda6c068b530059059b6bfc92319e04db
TAksenov <taksenov@gmail.com>
2016-12-15 18:45:08 +0500

    add ES6 version for classes and extends

--- calc/script.js
+++ calc/script.js
@@ -18,11 +18,105 @@
 //
 // Обратите внимание, что не должно быть дублирования кода из методов
 // оригинального калькулятора. Необходимо применить наследование.
-// TODO
 // Задачу необходимо выполнить в двух вариантах: ES5 и ES6
 
 // ES6 version =================================================================
 
+// Класс Calculator ============================================================
+class CalculatorES6 {
+    constructor(number) {
+        this.result = number;
+    }
+    // Сумма
+    sum() {
+        let methodInputValue = this.result;
+        for (let i = 0; i < arguments.length; i++) {
+            methodInputValue = methodInputValue + arguments[i];
+        }
+        return methodInputValue;
+    } //sum
+    // Вычитание
+    dif() {
+        let methodInputValue = this.result;
+        for (let i = 0; i < arguments.length; i++) {
+            methodInputValue = methodInputValue - arguments[i];
+        }
+        return methodInputValue;
+    }
+    //dif
+    // Произведение
+    mul() {
+        let methodInputValue = this.result;
+        for (let i = 0; i < arguments.length; i++) {
+            methodInputValue = methodInputValue * arguments[i];
+        }
+        return methodInputValue;
+    }
+    //mul
+    // Деление
+    div() {
+        let methodInputValue = this.result;
+        for (let i = 0; i < arguments.length; i++) {
+            if (  arguments[i] === 0 ) {
+                throw new Error('DIVIDE_BY_ZERO');
+            }
+            methodInputValue = methodInputValue / arguments[i];
+        }
+        return methodInputValue;
+    }
+    //div
+}
+// Класс Calculator ============================================================
+
+// Класс SqlCalc ===============================================================
+class SqlCalcES6 extends CalculatorES6 {
+    constructor(number) {
+        super(number);
+    }
+    sum() {
+        let methodInputValue = super.sum.apply(this, arguments);
+        return methodInputValue * methodInputValue;
+    } //sum
+    // Вычитание
+    dif() {
+        let methodInputValue = super.dif.apply(this, arguments);
+        return methodInputValue * methodInputValue;
+    };
+    //dif
+    // Произведение
+    mul() {
+        let methodInputValue = super.mul.apply(this, arguments);
+        return methodInputValue * methodInputValue;
+    };
+    //mul
+    // Деление
+    div() {
+        let methodInputValue = super.div.apply(this, arguments);
+        return methodInputValue * methodInputValue;
+    };
+    //div
+}
+// Класс SqlCalc ===============================================================
+
+let myCalculator = new SqlCalcES6(100);
+
+console.log('------[ES6 version]------');
+
+console.log(myCalculator.sum(1, 2, 3));
+console.log(myCalculator.dif(10, 20));
+console.log(myCalculator.div(2, 2));
+console.log(myCalculator.mul(2, 2));
+try {
+    console.log(myCalculator.div(2, 2, 3, 0, 5)); //вернет Ошибку Деления на ноль!
+} catch (e) {
+    if ( e.message === 'DIVIDE_BY_ZERO' ) {
+        console.log('Попытка поделить на ноль.');
+    } else {
+        console.log('Ошибка деления!');
+    }
+}
+
+console.log('------[ES6 version]------');
 // ES6 version =================================================================
 
 // ES5 version =================================================================
@@ -112,15 +206,14 @@ SqlCalc.prototype.div = function () {
 //div
 // Класс SqlCalc ===============================================================
 
-// let myCalculator2 = new Calculator(100);
 let myCalculator2 = new SqlCalc(100);
 
 console.log('------[ES5 version]------');
 
-console.log(myCalculator2.sum(1, 2, 3)); //вернет 106
-console.log(myCalculator2.dif(10, 20)); //вернет 70
-console.log(myCalculator2.div(2, 2)); //вернет 25
-console.log(myCalculator2.mul(2, 2)); //вернет 400
+console.log(myCalculator2.sum(1, 2, 3));
+console.log(myCalculator2.dif(10, 20));
+console.log(myCalculator2.div(2, 2));
+console.log(myCalculator2.mul(2, 2));
 try {
     console.log(myCalculator2.div(2, 2, 3, 0, 5)); //вернет Ошибку Деления на ноль!
 } catch (e) {
